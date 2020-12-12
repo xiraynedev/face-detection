8c52b8642b1db8e8c69a60d18fc74b10f90440f1
xiraynedev <xiraynedev@gmail.com>
2020-12-11 21:21:04 -0500

    initial upload

--- script.js
+++ script.js
@@ -8,24 +8,46 @@ Promise.all([
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+navigator.mediaDevices.getUserMedia({ video: {} })
+  .then(stream => {
+    video.srcObject = stream
+    video.onloadedmetadata = function(e) {
+      video.play()
+    }
+  }).catch(function(err) {
+    console.log(err.name)
+  })
 }
 
-video.addEventListener('play', () => {
+video.addEventListener('playing', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
+  const displaySize = { width: video.width, height: video.height } 
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+  })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
